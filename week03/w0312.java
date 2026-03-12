c8ef1b053842ab3817319731453ba1dd5708f336
ye1002 <C112181126@nkust.edu.tw>
2026-03-12 10:06:21 +0800

    456

---
+++ week03/w0312.java
@@ -0,0 +1,11 @@
+public class w0312{
+    public static void main(String[]args){
+        System.out.println("helloworld");
+    }
+}
+
+class myclass{
+    public void myclass_myfun(String[]args){
+        System.out.println("helloworld01");
+    }
+}
